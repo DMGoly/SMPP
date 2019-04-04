354f1a650f448eee125de502a3525bfb63af315a
dmgoly@gmail.com <Fredsq12wASD>
2019-04-04 17:55:03 +0300

    JavaFX interfaces

---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("SMPP Client created by DMGoly.fxml"));
+        primaryStage.setTitle("SMPP Client created by DMGoly");
+  //      primaryStage.setMinHeight(600);
+  //      primaryStage.setMaxHeight(800);
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
